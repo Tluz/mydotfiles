58ce11e7a367d33f24701d51ca5360db72dbb3f7
Tobias Queluz <tobias.queluz@grupoabl.com.br>
2025-07-02 18:14:45 -0300

    Verify installed packages (i3)
    - Changed the i3 script to verify if a package is installed in the system. If it is not, the script will install it.

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,31 +1,68 @@
+#!/bin/bash
+
+_isInstalled() {
+    package="$1";
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; 
+        return;
+    fi;
+    echo 1;
+    return;
+}
+
+_install() {
+    package="$1";
+    packageType=$2;
+
+    if [[ $(_isInstalled "${package}") == 0 ]]; then
+        echo "${package} is already installed.";
+        return;
+    fi;
+
+    if [packageType == 1 ]]; then
+        sudo pacman -S "${package}";
+        return;
+    fi;
+
+    yay -S ${package};
+}
+
 #nvim
 #sudo pacman -S neovim
-sudo cp -a ../config/nvim ~/.config/
+_install "neovim", 0;
+sudo cp -a ../config/nvim ~/.config/;
 
 #qutebrowser
 #sudo pacman -S qutebrowser
-sudo cp -a ../config/qutebrowser ~/.config/
+_install "qutebrowser", 0;
+sudo cp -a ../config/qutebrowser ~/.config/;
 
 #wallpaper
-#yay -S feh
-sudo cp -a ../wallpapers/background.png ~/Imagens/
-feh --bg-scale ~/Imagens/background.png
+#yay -S feh #Already installed with EndeavourOS i3
+#_install = "feh"
+sudo cp -a ../wallpapers/background.png ~/Imagens/;
+feh --bg-scale ~/Imagens/background.png;
 
 #starship
 #yay -S starship
-sudo cp -a ../config/config-schema.json ~/.config/
-sudo cp -a ../config/starship.toml ~/.config/
+_install "starship", 1
+sudo cp -a ../config/config-schema.json ~/.config/;
+sudo cp -a ../config/starship.toml ~/.config/;
 
-#rofi
-sudo cp -a ../config/rofi ~/.config/
+#rofi #Already installed with EndeavourOS i3
+sudo cp -a ../config/rofi ~/.config/;
 
-#i3
-sudo cp -a ../config/i3 ~/.config/
+#i3 #Already installed with EndeavourOS i3
+sudo cp -a ../config/i3 ~/.config/;
 
 #xfce
 #sudo pacman -S fastfetch
-sudo cp -a ../config/xfce4/current-color.theme /usr/share/xfce4/terminal/colorschemes/
+_install "fastfetch", 0;
+sudo cp -a ../config/xfce4/current-color.theme /usr/share/xfce4/terminal/colorschemes/;
 
 #polybar
 #sudo -S pacman tff-firacode-nerd
-sudo cp -a ../config/polybar ~/.config/
+_install "tff-firacode-nerd", 0;
+_install "polybar", 0;
+sudo cp -a ../config/polybar ~/.config/;
