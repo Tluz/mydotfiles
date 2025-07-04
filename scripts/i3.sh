1fdbff44fafc0e55d541084c8441bd61a7f5b653
Tobias Queluz <tobias.queluz@grupoabl.com.br>
2025-07-04 16:33:27 -0300

    Remove hyprland configuration
    - Hyprland files will be moved to a diferent repository

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -13,38 +13,33 @@ _isInstalled() {
 
 _install() {
   package="$1"
-  packageType=$2
 
   if [[ $(_isInstalled "${package}") == 0 ]]; then
     echo "${package} is already installed."
     return
   fi
 
-  if [[packageType == 1 ]]; then
-    echo "Installing ${package} via Yay"
-    yay -S ${package}
-    return
-  fi
-
-  echo "Installing ${package} via pacman"
-  sudo pacman -S "${package}"
+  echo "Installing ${package} via Yay"
+  yay -S ${package}
+  return
 }
 
 #nvim
-_install "neovim" 0
+_install "neovim"
 sudo cp -a ../config/nvim ~/.config/
 
 #qutebrowser
-_install "qutebrowser" 0
+_install "qutebrowser"
 sudo cp -a ../config/qutebrowser ~/.config/
 
 #wallpaper
 #_install "feh" 0 #Already installed with EndeavourOS i3
-sudo cp -a ../themes/catppuccin/background.png ~/Imagens/
-feh --bg-scale ~/Imagens/background.png
+mkdir -p ~/.local/share/wallpapers
+sudo cp -a ../config/background.png ~/.local/share/wallpapers/
+feh --bg-scale ~/.local/share/wallpapers/background.png
 
 #starship
-_install "starship" 0
+_install "starship"
 sudo cp -a ../config/config-schema.json ~/.config/
 sudo cp -a ../config/starship.toml ~/.config/
 
@@ -55,10 +50,10 @@ sudo cp -a ../config/rofi ~/.config/
 sudo cp -a ../config/i3 ~/.config/
 
 #xfce
-_install "fastfetch" 0
+_install "fastfetch"
 sudo cp -a ../config/xfce4/current-color.theme /usr/share/xfce4/terminal/colorschemes/
 
 #polybar
-_install "ttf-firacode-nerd" 0
-_install "polybar" 0
+_install "ttf-firacode-nerd"
+_install "polybar"
 sudo cp -a ../config/polybar ~/.config/
